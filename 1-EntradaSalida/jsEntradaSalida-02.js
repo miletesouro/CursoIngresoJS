19cb9c83cc02053334a0c6d39a48a4b0130d335a
Mile <mtesour99@gmail.com>
2021-02-01 14:43:56 -0300

    second commit

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,16 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombreDelAlumno;
+
+	nombreDelAlumno = prompt ("Ingrese su nombre.");
+
+
+	alert("Su nombre es: " + nombreDelAlumno + ". Bienvenido!");
 }
 
+//Entradas: prompt, getElementById
+//Procesos: -
+//Salidas: alert, console.log, document.getElementById, document.write
+// Al declarar la variable siempre hacerlo detalladamente para 
+//despues entenderlo bien. Ej: let nombreDelAlumno; let pesoDelPaciente;
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,7 +3,11 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-	alert("ok");
+{	//reservo espacio en memoria para guardar el nombre de usuario.
+	var nombreDeUsuario;
+	//guardo en la variable el texto que escribio el usuario dentro de la ventana prompt
+	nombreDeUsuario = prompt ("Ingrese su nombre.");
+	//copio el nombre que tengo guardado en la variable nombre dentro de la caja de texton embebida en la pagina html
+	document.getElementById("txtIdNombre").value = nombreDeUsuario ;
 }
 
