3d23b9b3eac12c80177e5d109c3428421eaceda5
Mile <mtesour99@gmail.com>
2021-02-03 23:36:00 -0300

    ejercicio de iva, bien!

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,73 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
 function Sumar () 
-{
+{   //txtIdPrecioUno
+    var suma;
+    var precioUno;
+    var precioDos;
+    var precioTres;
+
+    precioUno = document.getElementById('txtIdPrecioUno').value;
+    precioDos = document.getElementById('txtIdPrecioDos').value;
+    precioTres = document.getElementById('txtIdPrecioTres').value;
+
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+
+    suma = (precioUno + precioDos + precioTres);
+
+    alert("El resultado de la suma es " + suma);
 	
 }
 function Promedio () 
-{
-	
+{   //txtIdPrecioDos
+    var suma;
+    var promedio;
+    var precioUno;
+    var precioDos;
+    var precioTres;
+
+    precioUno = document.getElementById('txtIdPrecioUno').value;
+    precioDos = document.getElementById('txtIdPrecioDos').value;
+    precioTres = document.getElementById('txtIdPrecioTres').value;
+
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+
+    suma = (precioUno + precioDos + precioTres);
+    
+    promedio = (suma / 3);
+    
+    alert("El promedio es " + promedio);
 }
 function PrecioFinal () 
-{
+{   //txtIdPrecioTres
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var precioFinal;
+    var suma;
+    var aumentoIva;
+    var precioFinal;
+
+    precioUno = document.getElementById('txtIdPrecioUno').value;
+    precioDos = document.getElementById('txtIdPrecioDos').value;
+    precioTres = document.getElementById('txtIdPrecioTres').value;
+
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+   
+    suma = (precioUno + precioDos + precioTres);
+
+    aumentoIva = (suma * 21/100);
+    precioFinal = suma + aumentoIva;
+
+    alert("El precio final es " + precioFinal);
 	
 }
+
