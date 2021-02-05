11ab60c76c255803cd645caa0a13731bfe4458fa
Mile <mtesour99@gmail.com>
2021-02-05 10:02:11 -0300

    bien

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -3,11 +3,27 @@ A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en C
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
 function FahrenheitCentigrados () 
-{
+{   var temperaturaIngresada;
+    var temperaturaC;
+
+    temperaturaIngresada = document.getElementById('txtIdTemperatura').value;
+    temperaturaIngresada = parseInt(temperaturaIngresada);
+
+    temperaturaC = ((temperaturaIngresada -32)/1.8);
+
+    alert(temperaturaIngresada + "°F son " + temperaturaC + "°C.");
 	
 }
 
 function CentigradosFahrenheit () 
-{
+{   var temperaturaIngresada;
+    var temperaturaF;
+
+    temperaturaIngresada = document.getElementById('txtIdTemperatura').value;
+    temperaturaIngresada = parseInt(temperaturaIngresada);
+
+    temperaturaF = ((temperaturaIngresada * 1.8)+32);
+
+    alert(temperaturaIngresada + "°C son " + temperaturaF + "°F.");
 	
 }
